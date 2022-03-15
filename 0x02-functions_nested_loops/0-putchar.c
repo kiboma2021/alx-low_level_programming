81793a6ccd7ac98acad5861d77ce9e166e9101d6
Ben Kiboma <omayioben@gmail.com>
2022-03-15 20:34:30 +0300

    prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "main.h"
-/*
+/**
+ * main - starting point
  *
- * Return: Always 0 (Success)
+ * Return: return 0 if successful
  */
 int main(void)
 {
@@ -15,6 +16,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
