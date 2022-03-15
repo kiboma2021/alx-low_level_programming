c75bdc8dc1e9d47147958fee2df98f10ee6e65a1
Ben Kiboma <omayioben@gmail.com>
2022-03-15 23:50:29 +0300

    0x02

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 /**
  * print_last_digit - return last digit
  * @n : number to check
@@ -6,21 +6,14 @@
  */
 
 int print_last_digit(int n)
-
 {
-
 	int nv;
+
 	if (n < 0)
-	{
 		nv = -1 * (n % 10);
-	}
 	else
-	{
 		nv = n % 10;
-	}
 
 	_putchar(nv + '0');
-	return (nv);
-
+		return (nv);
 }
-
