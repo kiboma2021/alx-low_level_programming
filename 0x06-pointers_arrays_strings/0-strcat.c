e6f9ef3111b7e33ce2ed6cc3c6c573a976ee3bbf
Ben Kiboma <omayioben@gmail.com>
2022-03-24 23:09:34 +0300

    function that concatanates strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,33 +6,14 @@
  * @dest: destination string
  * @src: source string
  * Return: result of concatenation in string form
- *
- *char *_strcat(char *dest, char *src)
- *{
- *	int len_dest = strlen(dest);
- *
- *	for (let i = 0; i < strlen(src); i++)
- *		dest[len_dest + i] = src[i];
- *
- *	return (dest);
- *0}
  */
-
-#include<stdio.h>
-/**
- * A function that concatenates two strings
- * 
- * Always return 0
- * 
- */
-
-char *_strcat(char *dest,char *src)
-{
-  len_dest=strlen(dest);
-
-  for(let i=0;i<strlen(src);i++)
- 	 {
-	    dest[len_dest+i]=src[i];
-	 }
-  return(dest);
-}
+ char *_strcat(char *dest, char *src)
+ {
+ 	int len_dest = strlen(dest);
+ 
+ 	for (let i = 0; i < strlen(src); i++)
+ 		dest[len_dest + i] = src[i];
+ 
+ 	return (dest);
+ }
+ 
